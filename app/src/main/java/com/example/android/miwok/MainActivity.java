cb8a1c2f131e12e8205553e87e445fd9ca14bfaf
mslijepcevic <mslijepcevic@Marko-PC>
2020-04-15 15:20:22 +0200

    Swipe implemented.

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -20,22 +20,32 @@ import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    CategoryAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        // Create an adapter that knows which fragment should be shown on each page
+        mAdapter = new CategoryAdapter(getSupportFragmentManager());
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(mAdapter);
      }
 
-
+    /**
+     * This code snippet was used for the old layout where the user ha to click on a category
+     */
+    /*
     public void openNumbersList(View view) {
         Intent numberIntent = new Intent(this, NumbersActivity.class);
         startActivity(numberIntent);
@@ -55,5 +65,6 @@ public class MainActivity extends AppCompatActivity {
         Intent colorsIntent = new Intent(this, PhrasesActivity.class);
         startActivity(colorsIntent);
     }
+     */
 
 }
